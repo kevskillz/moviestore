b5a9ea2ae91a5eb0f330952ca91406d31d9acd3e
Aarav Mehta <aaravmehta@Aaravs-MacBook-Air-2.local>
2025-02-03 22:48:12 -0500

    Finished chapter 6

--- moviestore/movies/views.py
+++ moviestore/movies/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render
+from .models import Movie
 
-movies = [
-    {'id': 1, 'name': 'Inception', 'price': 12, 'description': 'A mind-bending heist thriller.'},
-    {'id': 2, 'name': 'Avatar', 'price': 13,
-     'description': 'A journey to a distant world and the battle for resources.'},
-    {'id': 3, 'name': 'The Dark Knight', 'price': 14, 'description': 'Gotham’s vigilante faces the Joker.'},
-    {'id': 4, 'name': 'Titanic', 'price': 11,
-     'description': 'A love story set against the backdrop of the sinking Titanic.'},
-]
+
 
 
 def index(request):
-    template_data = {'title': 'Movies', 'movies': movies}
+    search_term = request.GET.get('search')
+    if search_term:
+        movies = Movie.objects.filter(name__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+    template_data = {'title': 'Movies'}
+    template_data['movies'] = movies
     return render(request, 'movies/index.html', {'template_data': template_data})
 
 
 def show(request, id):
-    movie = next((m for m in movies if m["id"] == id), None)
+    movie = Movie.objects.get(id=id)
+    template_data = {}
     if not movie:
         return render(request, 'movies/index.html', {'error': 'Movie not found'})
 
-    template_data = {'title': movie['name'], 'movie': movie}
+    template_data['title'] = movie.name
+    template_data['movie'] = movie
     return render(request, 'movies/show.html', {'template_data': template_data})
--- moviestore/moviestore/moviestore/urls.py
+++ moviestore/moviestore/moviestore/urls.py
@@ -25,3 +25,4 @@ urlpatterns = [
     path('movies/', include('movies.urls')),
 
 ]
+
